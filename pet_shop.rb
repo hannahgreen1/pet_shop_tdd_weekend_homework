4a93054fef819d1d2c475f3cb4e521a8475fc708
hannahgreen1 <hannahgreen94@icloud.com>
2018-04-13 11:49:12 +0100

    4 tests passing

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,6 +2,10 @@ def pet_shop_name(shop)
   shop[:name]
 end
 
-def total_cash
- shop[:admin][:total_cash]
+def total_cash(shop)
+  shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(cash, total)
+  return cash[:admin][:total_cash] += total
 end
